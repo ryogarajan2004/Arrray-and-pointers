f16294ce8728bf3b5ef31ea9da550e7380ef93af
ryogarajan2004 <ryogarajan2004@gmail.com>
2022-10-27 18:47:13 +0530

    Git edited

--- Arrray and pointers/Arrray and pointers.cpp
+++ Arrray and pointers/Arrray and pointers.cpp
@@ -9,15 +9,7 @@ int main()
 	p = &x;
 	q = &p;
 	x = 5;
-	cout << "Value of x"<<setw(5) << x << endl;
-	cout << "Value of &x" <<setw(5)<< & x << endl;
-	cout << "Value of p" << setw(5)<<p << endl;
-	cout << "Value of *p" <<setw(5)<< * p << endl;
-	cout << "Value of &p" <<setw(5)<< & p << endl;
-	cout << "Value of q" <<setw(5)<< q << endl;
-	cout << "Value of &q" <<setw(5)<< & q << endl;
-	cout << "Value of *q" <<setw(5)<< * q << endl;
-	cout << "Value of **q" <<setw(5)<< * *q << endl;
+	cout << "Git New";
 	
 	
 }
